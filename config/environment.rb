e040bd330c165be6e460bb200801f9b8a66af5d2
kenjiro sugimoto <kenjirosugimoto@gmail.com>
2015-07-29 15:17:11 +0900

    Initialize repository

---
+++ config/environment.rb
@@ -0,0 +1,6 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+require 'tzinfo'
+Rails.application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '206722f099b6a620b1d2b39d34a2dcb0ab13acdc84db11f0fe3174d0c48a949303b5ddccc08d86535e142bea2ee0a9cffc4acd4fdc530e94e19dbf5a20f7d569'
